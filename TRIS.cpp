abb6a467be8006118dbdb7b4cf5b726aea7bd8d3
alicezampieri03 <76475213+alicezampieri03@users.noreply.github.com>
2020-12-23 10:29:13 +0100

    Update TRIS.cpp

--- TRIS.cpp
+++ TRIS.cpp
@@ -1,11 +1,13 @@
-// Lavoro di gruppo --> TRIS 
 
-#include<iostream>
-class Tris{
-  
-};
-
+#include <iostream>
+#define n 3
+using namespace std;
 int main(void){
-
-
+  int i=0, j=0, m[0][0];
+  for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+       		cout<<m[i][j]<<"\t";
+		}
+	}
+	
 }
