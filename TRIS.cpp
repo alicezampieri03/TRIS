d5a423f4f970c89f96658dbd9b06a86c0f6c26ce
alicezampieri03 <76475213+alicezampieri03@users.noreply.github.com>
2020-12-23 10:47:55 +0100

    Update TRIS.cpp

--- TRIS.cpp
+++ TRIS.cpp
@@ -1,13 +1,25 @@
 
+
 #include <iostream>
 #define n 3
 using namespace std;
-int main(void){
-  int i=0, j=0, m[0][0];
-  for(int i=0; i<n; i++){
+
+
+int matrice(void){
+ 	int i=0, j=0, m[0][0];
+  	for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
        		cout<<m[i][j]<<"\t";
 		}
 	}
+	
+}
+
+int main (void){
+	int mosse;
+	cout<<"dimmi la tua mossa: ";
+	cin<<mosse;
+	//estrai elemento dalla matrice 
+	
 	
 }
