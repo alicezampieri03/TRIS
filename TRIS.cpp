41de7d4182913629c8e5dbfb3e26e811f69cc15b
alicezampieri03 <76475213+alicezampieri03@users.noreply.github.com>
2020-12-26 11:28:25 +0100

    Update TRIS.cpp

--- TRIS.cpp
+++ TRIS.cpp
@@ -1,25 +1,40 @@
-
-
 #include <iostream>
-#define n 3
+#include <ctime>
+#include <cstdlib>
+#define r 3
+#define c 3
 using namespace std;
 
-
-int matrice(void){
- 	int i=0, j=0, m[0][0];
-  	for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-       		cout<<m[i][j]<<"\t";
-		}
-	}
-	
-}
-
 int main (void){
-	int mosse;
-	cout<<"dimmi la tua mossa: ";
-	cin<<mosse;
-	//estrai elemento dalla matrice 
+	int i=0, j=0, m[r][c], x,y;
+
+  	for (i=0; i<r; i++){
+        for (j=0; j<c; j++){
+            m[i][j] =0;
+            cout<<m[i][j]<<"  ";
+        }
+    cout<<endl;
+    }
+
+	cout<<"dove vuoi inserire la mossa(x): ";
+	cin>>x;
+	cout<<"dove vuoi inserire la mossa(y): ";
+	cin>>y;
 	
-	
+	if((m[i][j]==0)||(m[i][j]==0)){//controllo che la casella sia vuota
+ 		cout<<"Casella già occupata, inserisci di nuovo(x,y)";
+ 		cin>>x;
+		cin>>y;
+	}
+ 		
+	else if((x>2)||(x<0)||(y>2)||(y<0)){//controllo che le coordinate siano giuste
+ 		cout<<"Coordinate non valide, inserisci di nuovo(x,y)";
+		cin>>x;
+		cin>>y;
+	}
+	else{
+		m[y][x]='1';
+		cout<<m[i][j]<<" ";
+		cout<<endl;
+	}
 }
