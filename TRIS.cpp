3f817e3e29c1476a2d6e6b8e40ced0a8f87ed8c5
vitto3 <76210654+vitto3@users.noreply.github.com>
2020-12-27 10:29:25 +0100

    Update TRIS.cpp

--- TRIS.cpp
+++ TRIS.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 int main (void){
-	int i=0, j=0, m[r][c], x,y;
+	int i=0, j=0, m[r][c];
+	int x1, x2, y1, y2; 
 
   	for (i=0; i<r; i++){
         for (j=0; j<c; j++){
@@ -15,26 +16,53 @@ int main (void){
         }
     cout<<endl;
     }
-
-	cout<<"dove vuoi inserire la mossa(x): ";
-	cin>>x;
-	cout<<"dove vuoi inserire la mossa(y): ";
-	cin>>y;
+//Faccio il ciclo per riempire la nostra matrice con l'1 e il 2 
 	
-	if((m[i][j]==0)||(m[i][j]==0)){//controllo che la casella sia vuota
- 		cout<<"Casella già occupata, inserisci di nuovo(x,y)";
- 		cin>>x;
-		cin>>y;
+	for(i=0; i<m[x1][y1]; i++){
+		for(j=0; j<m[x2][y2]; j++){
+			cout<<"Dove vuoi inserire la mossa(x) Giocatore 1: ";
+			cin>>x1;
+			cout<<"Dove vuoi inserire la mossa(y) Giocatore 1: ";
+			cin>>y1;
+			cout<<"Dove vuoi inserire la mossa(x) Giocatore 2: ";
+			cin>>x2;
+			cout<<"Dove vuoi inserire la mossa(y) Giocatore 2: ";
+			cin>>y2;
+			
+			//chiedere le mosse al giocatore 1
+			if((m[i][j]==0)||(m[i][j]==0)){//controllo che la casella sia vuota
+		 		cout<<"Casella già occupata, inserisci di nuovo(x,y)";
+		 		cin>>x1;
+				cin>>y1;
+			}	
+			else if((x1>2)||(x1<0)||(y1>2)||(y1<0)){//controllo che le coordinate siano giuste
+		 		cout<<"Coordinate non valide, inserisci di nuovo(x,y)";
+				cin>>x1;
+				cin>>y1;
+			}
+				else{
+				m[y1][x1]='1';
+				cout<<m[i][j]<<" ";
+				cout<<endl;
+			}
+			
+			if((m[i][j]==0)||(m[i][j]==0)){//controllo che la casella sia vuota
+		 		cout<<"Casella già occupata, inserisci di nuovo(x,y)";
+		 		cin>>x2;
+				cin>>y2;
+			}
+		 	// Chiedere le mosse al giocatore 2 
+			else if((x2>2)||(x2<0)||(y2>2)||(y2<0)){//controllo che le coordinate siano giuste
+		 		cout<<"Coordinate non valide, inserisci di nuovo(x,y)";
+				cin>>x2;
+				cin>>y2;
+			}
+				else{
+				m[y2][x2]='2';
+				cout<<m[i][j]<<" ";
+				cout<<endl;
+			}
+		}
 	}
- 		
-	else if((x>2)||(x<0)||(y>2)||(y<0)){//controllo che le coordinate siano giuste
- 		cout<<"Coordinate non valide, inserisci di nuovo(x,y)";
-		cin>>x;
-		cin>>y;
-	}
-	else{
-		m[y][x]='1';
-		cout<<m[i][j]<<" ";
-		cout<<endl;
-	}
+	
 }
