de521c0b3afa06b8eab9952ae0af85e87a2d7fd5
alicezampieri03 <76475213+alicezampieri03@users.noreply.github.com>
2021-01-17 10:42:00 +0100

    Update TRIS.cpp

--- TRIS.cpp
+++ TRIS.cpp
@@ -19,25 +19,30 @@ class Tris{
 		int getY(void);
 		int controllo_v();
 		
-};
+};*/
 
-void griglia(char m[3][3]){
-	int i=0, j=0;
+void griglia(char matrix[][3]){
     cout << "tris"<<endl;
-    cout << "giocatore 1(X)  -  giocatore 2(O)" << endl;
+    
+    cout << "giocatore 1(X)  -  giocatore 2(O)" << endl << endl;
+    cout << endl;
 
     cout << "     |     |     " << endl;
-    cout << "  " <<m[i-1][j-1]<< "  |  " <<m[i-1][j]<< "  |  " <<m[i-1][j+1]<< endl;
+    cout << "  " << matrix[0][0] << "  |  " << matrix[0][1] << "  |  " << matrix[0][2] << endl;
+
     cout << "_____|_____|_____" << endl;
     cout << "     |     |     " << endl;
- 	cout << "  " <<m[i][j-1]<< "  |  " <<m[i][j]<< "  |  " <<m[i][j+1]<< endl;  
-	cout << "_____|_____|_____" << endl;
+
+   	cout << "  " << matrix[1][0] << "  |  " << matrix[1][1] << "  |  " << matrix[1][2] << endl;
+
+    cout << "_____|_____|_____" << endl;
     cout << "     |     |     " << endl;
-    cout << "  " <<m[i+1][j-1]<< "  |  " <<m[i+1][j]<< "  |  " <<m[i+1][j+1]<< endl;
+
+    cout << "  " << matrix[2][0] << "   |  " << matrix[2][1] << "   |  " << matrix[2][2] << endl;
+
     cout << "     |     |     " << endl << endl;
 }
 
-*/
 int controllo_v(char m[3][3]){
 	int i=0, j=0;
  	for(i=0;i<3;i++){
@@ -89,7 +94,7 @@ int controllo_v(char m[3][3]){
 	} 
 }
 
-int matrice_vuota(char m[3][3]){  
+int stringa_vuota(char m[3][3]){  
     //cout<<"griglia vuota"<<endl;
     int i=0, j=0;
 	for (i=0; i<3; i++) {
@@ -101,63 +106,70 @@ int matrice_vuota(char m[3][3]){
 	}
 }
 
-int visualizza_matrice(char m[3][3]){
-	int i=0, j=0;
-	cout<<m[i][j]<<" ";
-	cout<<endl;
-}
-
-
-
 int controllo_v(char m[3][3]);
-int matrice(char m[3][3]);
-int visualizza_matrice(char m[3][3]);
-void griglia(char m[3][3]);
+int stringa_vuota(char m[3][3]);
+void griglia();
 
 
 int main(void){
-	char m[3][3];
+	char m[3][3] = {{'\0'}}; // V = vuoto
 	int i=0, j=0, conta=0, vitt=0, x, y;
 	char giocatore1[10], giocatore2[10];
-	
+	bool exit = false;
 	fstream file;
-	file.open("PartitaTris.txt", ios::in);
+	file.open("PartitaTris.txt", ios::out);
 	
-	
-//	cout<<"griglia vuota"<<endl;
-//	griglia(m);
+	//cout<<"Griglia vuota"<<endl;
 
 	cout<<"giocatore 1 inserisci il nome: ";
+	cin>>giocatore1;
 	file>>giocatore1;
-	cout<<giocatore1<< "usa la X" <<endl;
+	cout<<giocatore1<<" usa la X" <<endl;
 	cout<<"giocatore 2 inserisci il nome: ";
+	cin>>giocatore2;
 	file>>giocatore2;
 	cout<<giocatore2<<" usa la O"<<endl;
 	
 	do{
-		cout<<giocatore1<<"inserisci la X (x,y): "<<endl;
+		cout<<giocatore1<<" dove vuoi inserire la X (x,y) : ";
 		cin>>x;
 		cin>>y;
-		if ((m[i][j]=='X')||(m[i][j]=='O')||(x>2)||(x<0)||(y>2)||(y<0)||m[i][j]==' '){
-			cout<<"errore riprova"<<endl;
-		}else{
+		cout<<"La x che hai inserito vale: "<<x<<endl;
+		cout<<"La y che hai inserito vale: "<<y<<endl;
+		if((m[i][j]=='X')||(m[i][j]=='O')||(x>2)||(x<0)||(y>2)||(y<0)){//controllo che la casella selezionata sia effettivamente vuota
+			cout<<"Errore, riprova";
+		}
+		else{
 			m[x][y]='X';
-			visualizza_matrice(m);
-			conta++;
+			conta++;//contatore delle caselle riempite
+		}
+		system("cls");//pulisce lo schemro
+		griglia(m);
+		if (conta == 9){
+			exit = true; // se diventa vero ho riempito tutte le caselle e quindi esco dal ciclo
 		}
 		
-		cout<<giocatore2<<"inserisci la O (x,y): "<<endl;
+		cout<<giocatore2<<" dove vuoi inserire la O (x,y) : ";
 		cin>>x;
 		cin>>y;
-		if ((m[i][j]=='X')||(m[i][j]=='O')||(x>2)||(x<0)||(y>2)||(y<0)||m[i][j]==' '){
-			cout<<"errore riprova"<<endl;
-		}else{
-			m[x][y]='O';
-			visualizza_matrice(m);
-			conta++;
+		cout<<"La x che hai inserito vale: "<<x<<endl;
+		cout<<"La y che hai inserito vale: "<<y<<endl;
+		
+		if((m[i][j]=='X')||(m[i][j]=='O')||(x>2)||(x<0)||(y>2)||(y<0)){//controllo che la casella selezionata sia effettivamente vuota
+			cout<<"Errore, riprova";
 		}
-	}while(conta<=9);
-
+		else{
+			m[x][y]='O';
+			conta++;//contatore delle caselle riempite
+		}
+		system("cls");
+		griglia(m);
+		if (conta == 9){
+			exit = true; // se diventa vero ho riempito tutte le caselle e quindi esco dal ciclo
+		}
+		
+	}while(!exit);//ripeto finchè non riempio le caselle
+	
 	vitt=controllo_v(m);
 	
 	if (vitt==1){
